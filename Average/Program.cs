4a646de5465c592e499959a0173035bdd471d03d
Ольга Черепанова <chernichka.ekb@gmail.com>
2021-11-20 15:28:27 +0500

    Добавьте файлы проекта.

---
+++ Average/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] Mass = new double[7];
+            Mass[0] = Convert.ToDouble(Console.ReadLine());
+            Mass[1] = Convert.ToDouble(Console.ReadLine());
+            Mass[2] = Convert.ToDouble(Console.ReadLine());
+            Mass[3] = Convert.ToDouble(Console.ReadLine());
+            Mass[4] = Convert.ToDouble(Console.ReadLine());
+            Mass[5] = Convert.ToDouble(Console.ReadLine());
+            Mass[6] = Convert.ToDouble(Console.ReadLine());
+            double Sum = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                Sum += Mass[i];
+            }
+            Console.WriteLine("Среднее арифметическое {0,4:f2}", Sum / 7);
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Tyc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое положительное число ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,]Tyc= new int [N, N];
+            int count = 0;
+            
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    count++;
+                    Tyc[i, j] = count % 2;
+                     Console.Write("{0} ", Tyc[i, j]);
+
+                    
+
+
+                }Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
